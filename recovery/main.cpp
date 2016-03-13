3ff20aadc1117dd2acb162bf9242a07bd7e08aa5
procount <kevin.procount@googlemail.com>
2016-03-13 00:15:29 +0000

    Allow use with Gerts VGA666 adaptor
    updated README.md, added changes.md
    
    Conflicts:
    	recovery/main.cpp

--- recovery/main.cpp
+++ recovery/main.cpp
@@ -132,10 +132,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     RightButtonFilter rbf;
     LongPressHandler lph;
-    GpioInput gpio(gpioChannel);
+    GpioInput *gpio = NULL;
 
     bool runinstaller = false;
-    bool gpio_trigger = false;
+
     bool keyboard_trigger = true;
     bool force_trigger = false;
 
@@ -152,7 +152,7 @@ int main(int argc, char *argv[])
             runinstaller = true;
         // Enables use of GPIO 3 to force NOOBS to launch by pulling low
         else if (strcmp(argv[i], "-gpiotriggerenable") == 0)
-            gpio_trigger = true;
+            gpio = new GpioInput(gpioChannel);
         // Disables use of keyboard to trigger recovery GUI
         else if (strcmp(argv[i], "-keyboardtriggerdisable") == 0)
             keyboard_trigger = false;
@@ -250,7 +250,7 @@ int main(int argc, char *argv[])
     // or no OS is installed (/settings/installed_os.json does not exist)
     bool bailout = !runinstaller
         && !force_trigger
-        && !(gpio_trigger && (gpio.value() == 0 ))
+        && !(gpio && (gpio->value() == 0 ))
         && hasInstalledOS(drive);
 
     if (bailout && keyboard_trigger)
