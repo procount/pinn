84f78f3eda508a8a63dedc3949f0ca2b89b67587
procount <kevin.procount@googlemail.com>
2019-10-30 14:44:57 +0000

    busyboc-dev -> busybocdev

--- buildroot/package/busybocdev/busyboc-dev.mk
+++ buildroot/package/busybocdev/busyboc-dev.mk
@@ -4,27 +4,26 @@
 #
 #############################################################
 
-BUSYBOC-DEV_VERSION = 1.0
-BUSYBOC-DEV_SITE = $(TOPDIR)/../busyboc-dev
-BUSYBOC-DEV_SITE_METHOD = local
-BUSYBOC-DEV_LICENSE = BSD-3c
-BUSYBOC-DEV_LICENSE_FILES = LICENSE.txt
-BUSYBOC-DEV_INSTALL_STAGING = NO
+BUSYBOCDEV_VERSION = 1.0
+BUSYBOCDEV_SITE = $(TOPDIR)/../busybocdev
+BUSYBOCDEV_SITE_METHOD = local
+BUSYBOCDEV_LICENSE = BSD-3c
+BUSYBOCDEV_LICENSE_FILES = LICENSE.txt
+BUSYBOCDEV_INSTALL_STAGING = NO
 
-define BUSYBOC-DEV_BUILD_CMDS
+define BUSYBOCDEV_BUILD_CMDS
 	cd $(@D) 
 	$(MAKE) -C $(@D) all
 	$(TARGET_STRIP) $(@D)/busyboc
 
 endef
 
-define BUSYBOC-DEV_INSTALL_TARGET_CMDS
+define BUSYBOCDEV_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 04755 $(@D)/busyboc $(TARGET_DIR)/bin/busyboc
 	ln -sf ../../bin/busyboc $(TARGET_DIR)/usr/bin/tee
 endef
 
 $(eval $(generic-package))
-#$(eval $(host-generic-package))
 
 
 
