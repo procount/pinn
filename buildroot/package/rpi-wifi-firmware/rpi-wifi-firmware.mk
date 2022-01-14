8b92d5d2ce2375470362113554c1010bea968563
procount <kevin.procoun@googlemail.com>
2022-01-14 17:22:53 +0000

    p3.8 NOOBS update. FW loading, bump kernel/firmware. New wifi firmware packaging

--- buildroot/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ buildroot/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -6,29 +6,35 @@
 
 #RPI_WIFI_FIRMWARE_VERSION = 7533cd1f124f07d87ca6fd11a4a2143748ed806c
 #RPI_WIFI_FIRMWARE_VERSION = b66ab26cebff689d0d3257f56912b9bb03c20567
-RPI_WIFI_FIRMWARE_VERSION = f713a6054746bc61ece1c8696dce91a7b7e22dd9
-
-RPI_WIFI_FIRMWARE_SITE = $(call github,RPi-Distro,firmware-nonfree,$(RPI_WIFI_FIRMWARE_VERSION))
+#RPI_WIFI_FIRMWARE_VERSION = f713a6054746bc61ece1c8696dce91a7b7e22dd9
+#RPI_WIFI_FIRMWARE_SITE = $(call github,RPi-Distro,firmware-nonfree,$(RPI_WIFI_FIRMWARE_VERSION))
+RPI_WIFI_FIRMWARE_VERSION = 20210315-3+rpt4
+RPI_WIFI_FIRMWARE_SITE = http://archive.raspberrypi.org/debian/pool/main/f/firmware-nonfree/
+RPI_WIFI_FIRMWARE_SOURCE = firmware-brcm80211_$(RPI_WIFI_FIRMWARE_VERSION)_all.deb
 RPI_WIFI_FIRMWARE_LICENSE = Proprietary
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
+
+define RPI_WIFI_FIRMWARE_EXTRACT_CMDS
+	dpkg-deb -x $(DL_DIR)/$(RPI_WIFI_FIRMWARE_SOURCE) $(@D)
+endef
 
 define RPI_WIFI_FIRMWARE_INSTALL_TARGET_CMDS
 
     #BRCM
-	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43143.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43143.bin
-	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43430-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.bin
-	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43430-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.txt
 
-        $(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43436-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43436-sdio.bin
-        $(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43436-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43436-sdio.txt
-        $(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43436-sdio.clm_blob $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43436-sdio.clm_blob
-        $(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43455-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.bin
-        $(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43455-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.txt
-        $(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43455-sdio.clm_blob $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob
-
-	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43456-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43456-sdio.bin
-	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43456-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43456-sdio.txt
-	$(INSTALL) -D -m 0644 $(@D)/brcm/brcmfmac43456-sdio.clm_blob $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43456-sdio.clm_blob
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43143.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43143.bin
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43430-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.bin
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43430-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.txt
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43430-sdio.clm_blob $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43430-sdio.clm_blob
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43436-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43436-sdio.bin
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43436-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43436-sdio.txt
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43436-sdio.clm_blob $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43436-sdio.clm_blob
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43455-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.bin
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43455-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.txt
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43456-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43456-sdio.bin
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43456-sdio.txt $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43456-sdio.txt
+	$(INSTALL) -D -m 0644 $(@D)/lib/firmware/brcm/brcmfmac43456-sdio.clm_blob $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43456-sdio.clm_blob
 
     #RALINK
 	$(INSTALL) -D -m 0644 $(@D)/rt3290.bin  $(TARGET_DIR)/lib/firmware/rt3290.bin
