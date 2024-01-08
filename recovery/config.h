cba7e1e93574f1aff6dc744040ece9da78dd7110
procount <kevin.procount@googlemail.com>
2024-01-08 10:16:51 +0000

    3.8.7c Fixed Set Date and Time tooltip

--- recovery/config.h
+++ recovery/config.h
@@ -3,7 +3,7 @@
 
 
 /* Version number displayed in the title bar */
-#define VERSION_NUMBER "3.8.7a"
+#define VERSION_NUMBER "3.8.7c"
 
 /* Color of the background */
 // #define BACKGROUND_COLOR  Qt::white
