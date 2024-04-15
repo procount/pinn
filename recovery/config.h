d3e0ebccbcf4cd0ecdfd25e553f6cc71fbb30ade
procount <kevin.procount@googlemail.com>
2024-04-15 09:37:52 +0100

    3.8.9j beta

--- recovery/config.h
+++ recovery/config.h
@@ -3,7 +3,7 @@
 
 
 /* Version number displayed in the title bar */
-#define VERSION_NUMBER "3.8.9i"
+#define VERSION_NUMBER "3.8.9j"
 
 /* Color of the background */
 // #define BACKGROUND_COLOR  Qt::white
